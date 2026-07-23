055d4d13b681a2b28129779e4484c7b2336cd237
Rithiikk <ritthik.moorthy@zucisystems.com>
2026-07-23 21:05:09 +0530

    Added admin login module

--- CricInfo.API/Program.cs
+++ CricInfo.API/Program.cs
@@ -1,22 +1,39 @@
+using CricInfo.Application.Interfaces.Services.AdminLogin;
 using CricInfo.Application.Interfaces.Services.LiveModule;
 using CricInfo.Application.Interfaces.Services.PointsTableModule;
+
 using CricInfo.Application.Mapping.CompletedModule;
+
+using CricInfo.Application.Services.AdminLoginModule;
 using CricInfo.Application.Services.PointsTableModule;
+
 using CricInfo.Infrastructure;
 using CricInfo.Infrastructure.presistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
 builder.Services.AddDbContext<CricDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultString")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultString")));
+
 builder.Services.AddInfrastructure();
+
 builder.Services.AddAutoMapper(typeof(ILiveService).Assembly);
-builder.Services.AddAutoMapper(typeof(CompletedMappingProfile).Assembly);
+
+builder.Services.AddAutoMapper(
+    typeof(CompletedMappingProfile).Assembly);
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AngularPolicy", policy =>
@@ -27,19 +44,54 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod();
     });
 });
+
+// PointStable Services
+
 builder.Services.AddScoped<
     IPointsTableService,
     PointsTableService>();
+
+// Admin Login Service
+
+builder.Services.AddScoped<
+    IAuthService,
+    AuthService>();
+builder.Services.AddScoped<EmailService>();
+
+// JWT Authentication
+
+builder.Services
+    .AddAuthentication(
+        JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(
+                            builder.Configuration["Jwt:Key"]!))
+            };
+    });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
 app.UseCors("AngularPolicy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ CricInfo.Application/Interfaces/Services/AdminLogin/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using CricInfo.Application.DTOs.AdminLogin;
+
+namespace CricInfo.Application.Interfaces.Services.AdminLogin;
+
+public interface IAuthService
+{
+    Task<LoginResponseDto?> Login(LoginDto dto);
+
+    Task<bool> ResetPassword(
+        ResetPasswordDto dto);
+
+    Task<ProfileDto?> GetProfile(
+        string username);
+
+    Task<bool> UpdateProfile(
+     int id,
+     ProfileDto dto);
+    Task<bool> Register(RegisterDto dto);
+}
